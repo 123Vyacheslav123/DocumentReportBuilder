d486bee1917fc8507d8b3c2eb6327de6fc84e45b
maxgotech <masjanja24122002@mail.ru>
2022-03-27 20:38:09 +0300

    Исправлена ошибка с регистрацией + убраны ненужные пактеы nuget

--- Reg.aspx.cs
+++ Reg.aspx.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using DocumentReportBuilder.Functions;
-using MongoDB.Driver;
 
-namespace Registration
+namespace DocumentReportBuilder
 {
     public partial class Reg : System.Web.UI.Page
     {
@@ -30,16 +28,6 @@ namespace Registration
 
         protected  void ButtonRegistration_Click(object sender, EventArgs e)
         {
-            /////Подключение БД
-            MongoClient client = new MongoClient(
-            ConfigurationManager.AppSettings["connectionString"]);
-            MongoServer server = client.GetServer();
-
-            string databaseName = "ReportBuilderDB"; // Имя БД
-            string collectionName = "Users"; // Имя коллекции с пользователями
-
-            MongoDatabase db = server.GetDatabase(databaseName);
-            MongoCollection<PersonModel> collection = db.GetCollection<PersonModel>(collectionName);
             
         }
     }
