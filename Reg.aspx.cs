ff7144667a7eb184f4036ba62d48b370212f54c6
Maksim Golovin <mgolovin@sfedu.ru>
2022-03-30 18:25:53 +0300

    Проверка на корректность регистрации и проверка на уже существующую почту в бд

--- Reg.aspx.cs
+++ Reg.aspx.cs
@@ -42,8 +42,40 @@ namespace DocumentReportBuilder
             {
                 TypeOfAccount = "Student";
             }
-            SqlCommand cmd = new SqlCommand("INSERT INTO [USERS]([Firstname],[Surname],[Patronymic],[Mail],[Typeofaccount],[Password]) VALUES('"+TextBoxFirstNameReg.Text+"','"+TextBoxLastNameReg.Text+"','"+ TextBoxPatronymicReg.Text+ "','"+TextBoxMailReg.Text+ "','"+ TypeOfAccount + "','"+TextBoxPassReg.Text+"')", con);
-            cmd.ExecuteNonQuery();
+
+            bool mailcheck;
+            string sqlCheckMail = "IF NOT('"+TextBoxMailReg.Text+"' LIKE '%[A-Z0-9][@][A-Z0-9]%[.][A-Z0-9]%' AND '"+TextBoxMailReg.Text+"' NOT LIKE '%[ <> ]%' BEGIN SELECT 'FALSE' END ELSE BEGIN SELECT 'TRUE' END)";
+            SqlCommand sqlmailcheck = new SqlCommand(sqlCheckMail,con);
+            mailcheck = Convert.ToBoolean(sqlmailcheck.ExecuteScalar());
+
+
+            if (mailcheck == true) //// проверка на корректность почты
+            {
+                bool status;
+                string sqlExistsMail = "IF EXISTS(SELECT [Mail] FROM [USERS] WHERE [Mail] = '" + TextBoxMailReg.Text + "') BEGIN SELECT 'TRUE' END ELSE BEGIN SELECT 'FALSE' END";
+                SqlCommand mailexists = new SqlCommand(sqlExistsMail, con);
+                status = Convert.ToBoolean(mailexists.ExecuteScalar());
+
+                
+                if (status == false) ///////////// проверка на наличие уже зарегистрированной почты
+                {
+                    /////////////// вставка пользователя в бд
+                    string SqlInsertUser = "INSERT INTO[USERS]([Firstname],[Surname],[Patronymic],[Mail],[Typeofaccount],[Password]) VALUES('" + TextBoxFirstNameReg.Text + "', '" + TextBoxLastNameReg.Text + "', '" + TextBoxPatronymicReg.Text + "', '" + TextBoxMailReg.Text + "', '" + TypeOfAccount + "', '" + TextBoxPassReg.Text + "')";
+                    SqlCommand cmd = new SqlCommand(SqlInsertUser, con);
+                    cmd.ExecuteNonQuery();
+                }
+                else
+                {
+                    ////  если такая почта существует в базе
+                }
+
+            }
+            else
+            {
+                ////// если почта не корректна
+
+            }
+
             con.Close();
         }
     }
