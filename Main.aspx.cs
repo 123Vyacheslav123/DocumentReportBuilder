2d59dd19d1742fad0070f2254418d4923940b36c
Maksim Golovin <mgolovin@sfedu.ru>
2022-04-11 15:13:11 +0300

    Добавлена страница MAIN

---
+++ Main.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
+namespace DocumentReportBuilder
+{
+    public partial class Main : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+
+            Thread t = new Thread(new ThreadStart(() =>
+            {
+                var fileDialog = new OpenFileDialog();
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+
+                }
+
+            }));
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();
+
+
+
+
+        }
+    }
+}
